be57db3d69187f2de1f87d630561e5bf6a59ebdf
Matt Brown <matt@mattb.net.nz>
2022-12-02 21:05:27 +1300

    Day 2. Puzzle 1.
    Fixed. Of course... rock has to beat scissors!
    
    Another 17 minutes.

--- 2022/2/rps.go
+++ 2022/2/rps.go
@@ -14,38 +14,39 @@ import (
 )
 
 const (
-  Undefined int = iota
-  Rock
-  Paper
-  Scissors
+	Undefined int = iota
+	Rock
+	Paper
+	Scissors
 )
 
 var Codes map[string]int = map[string]int{"A": Rock, "B": Paper, "C": Scissors, "X": Rock, "Y": Paper, "Z": Scissors}
+var Names map[int]string = map[int]string{Rock: "Rock", Paper: "Paper", Scissors: "Scissors"}
 
+const Loss = 0
 const Draw = 3
 const Win = 6
+
+type BeatTable map[int]map[int]int
+
+var Beats BeatTable = BeatTable{
+	Rock:     {Rock: Draw, Paper: Win, Scissors: Loss},
+	Paper:    {Rock: Loss, Paper: Draw, Scissors: Win},
+	Scissors: {Rock: Win, Paper: Loss, Scissors: Draw},
+}
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	sum := 0
 	for s.Scan() {
-    plays := strings.Split(s.Text(), " ")
-    them := Codes[plays[0]]
-    me := Codes[plays[1]]
-    if them == 0 || me == 0 {
-      log.Fatal("unknown play %s", s.Text())
-    }
-    if me > them {
-      fmt.Print(plays, " win ", me, " ", Win)
-      sum += me + Win
-    } else if me == them {
-      fmt.Print(plays, " draw ", me, " ", Draw)
-      sum += me + Draw
-    } else {
-      fmt.Print(plays, " loss ", me)
-      sum += me
-    }
-    fmt.Println(" sum ", sum)
+		plays := strings.Split(s.Text(), " ")
+		them := Codes[plays[0]]
+		me := Codes[plays[1]]
+		if them == 0 || me == 0 {
+			log.Fatalf("unknown play %s", s.Text())
+		}
+		sum += me + Beats[them][me]
+		fmt.Printf("%v %s vs %s, +%d +%d = %d\n", plays, Names[them], Names[me], me, Beats[them][me], sum)
 	}
 
 	fmt.Println(sum)
